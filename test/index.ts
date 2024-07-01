1c629fde1b9347acdb97a225ec18b6488aca2379
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-02 00:16:56 +0100

    update test

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,5 @@ import FNLB from '../src/index';
 const fnlb = new FNLB();
 
 await fnlb.start({
-	apiToken: process.env.API_TOKEN ?? '',
-	numberOfShards: 1,
-	categories: process.env.CATEGORIES?.split(',')
+	apiToken: process.env.API_TOKEN as string
 });
